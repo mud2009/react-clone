cda7f72381ff7c1255dd3adcb66e6c2f64ca8d22
Will Greenberg <greenberg.will@gmail.com>
2022-04-18 13:23:19 -0700

    add profile component, add columns for each component

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,28 @@
 import React from "react";
 import Navbar from "./Navbar.js"
 import Feed from "./Feed.js"
+import Profile from "./Profile.js"
 
 function App(){
   return ( 
     <React.Fragment>
       <Navbar />
-      <div style={{
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
-          flexDirection: 'column'
-      }}>
-        <Feed />
+      <div className="row">
+        <div className="col-4">
+          <Profile />
+        </div>
+        {/* <div style={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            flexDirection: 'column'
+        }}> */}
+        <div className="col-4">
+          <Feed />
+        </div>
+        <div className="col-4">
+          
+        </div>
       </div>
     </React.Fragment>
   );
